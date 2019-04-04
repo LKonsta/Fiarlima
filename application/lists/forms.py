96209dc44c16946d4baacd7edd813e2ac7a564d5
LKonsta <komppa.lehto@gmail.com>
2019-04-04 23:23:24 +0300

    Working lists with some army data commit

--- application/lists/forms.py
+++ application/lists/forms.py
@@ -5,8 +5,8 @@ from application.armydata.models import ArmyType
 
 class ListsForm(FlaskForm):
 	list = []
-	for army in ArmyType.query.all():
-		list.append(army.id)
+#	for army in ArmyType.query.all():
+#		list.append(army.id)
 
 	army_id = IntegerField()
 	name = StringField("List name", [validators.length(min=2)])
